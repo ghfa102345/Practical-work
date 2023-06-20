67d8d8dbb2efd4ff2cf8d5aae3cdff35d32d00c7
ghfa102345 <ghfa102345@gmail.com>
2023-06-20 13:43:25 +0300

    test-20

---
+++ js/steps-card-grid.js
@@ -0,0 +1,8 @@
+// const gridContainer = document.querySelector('.steps__cards');
+// const gridItemCount = gridContainer.children.length;
+
+// if (gridItemCount >= 5) {
+//     gridContainer.style.gridTemplateColumns = 'repeat(auto-fit, minmax(250px, 1fr))';
+// } else {
+//     gridContainer.style.gridTemplateColumns = 'repeat(auto-fit, 280px)';
+// }
